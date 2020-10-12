a3f71d3c4d372ab8a6d77f12657681bee25f3308
Pablo Menz <p.menz.r@gmail.com>
2020-10-12 19:51:43 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
   def change
     add_columnm :students, :grade, :integer
     add_column :students, :birthday, :string
