909cb49255c70d1b1828ee3a648839690a84bde7
Pablo Menz <p.menz.r@gmail.com>
2020-10-12 19:52:10 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,6 +1,8 @@
-class CreateStudents < ActiveRecord::Migration[5.2]
-  def change
-    create_table :students do |t|
-      t.string :name
-    end
+
+class CreateStudents < ActiveRecord::Migration[5.1]
+def change
+  create_table :students do |t|
+    t.string :name
+  end
+end
 end
